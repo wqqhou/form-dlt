4cdf3fc3c93f9e7174b22fabae93900808e32118
wqqhou <30869335+wqqhou@users.noreply.github.com>
2020-04-07 01:07:32 +0800

    Update judge.blade.php

--- resources/views/test/judge.blade.php
+++ resources/views/test/judge.blade.php
@@ -4,21 +4,11 @@
     <div class="container">
         <div class="jumbotron">
             <h3>測試結果</h3>
-            @if($point==100)
+            
                 <h4>感謝您的填寫!!</h4>
-            @elseif($point>=85)
-                <h4>Great!您得了{{$point}}分！</h4>
-            @elseif($point>=60)
-                <h4>Good!您得了{{$point}}分！</h4>
-            @else
-                <h4>繼續努力!您得了{{$point}}分！</h4>
-            @endif
-
-            @if($point==100)
+                
                 <a class="text-warning" href={{"/test/$test_id/alldetails"}}>點擊查看答題詳情</a>
-            @else
-                <a class="text-danger" href={{"/test/$test_id/details"}}>點擊查看錯題詳情</a>
-            @endif
+            
         </div>
     </div>
 @endsection
