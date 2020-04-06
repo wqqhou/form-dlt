a93d66b3549de646f9f0302347393e244536ded0
wqqhou <30869335+wqqhou@users.noreply.github.com>
2020-04-07 00:18:45 +0800

    Update judge.blade.php

--- resources/views/test/judge.blade.php
+++ resources/views/test/judge.blade.php
@@ -13,7 +13,6 @@
             @else
                 <h4>繼續努力!您得了{{$point}}分！</h4>
             @endif
-            <h4>一共{{$total}}道題，您答對了{{array_sum($answer)}}道</h4>
 
             @if($point==100)
                 <a class="text-warning" href={{"/test/$test_id/alldetails"}}>點擊查看答題詳情</a>
