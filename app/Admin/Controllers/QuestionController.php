eaaee3d27099cd0d0e5c03bc2b25ad9b53b9b378
wqqhou <30869335+wqqhou@users.noreply.github.com>
2020-04-06 22:15:43 +0800

    Update QuestionController.php

--- app/Admin/Controllers/QuestionController.php
+++ app/Admin/Controllers/QuestionController.php
@@ -189,8 +189,8 @@ class QuestionController extends Controller
             $form->display('id', 'ID');
             $form->hidden('qtype_id', 'Question Type')->default(1);
             $form->ckeditor('title', '題目');
-            $form->textarea('options', '選項')->default("A.選項內容\nB.選項內容\nC.選項內容\nD.選項內容\nE.選項內容");
-            $form->select('answer', '答案')->options(['A' => 'A', 'B' => 'B', 'C' => 'C', 'D' => 'D', 'E' => 'E']);
+            $form->textarea('options', '選項')->default("A.選項內容\nB.選項內容\nC.選項內容\nD.選項內容\nE.選項內容\nF.選項內容\nG.選項內容\nH.選項內容\nI.選項內容");
+            $form->select('answer', '答案')->options(['A' => 'A', 'B' => 'B', 'C' => 'C', 'D' => 'D', 'E' => 'E', 'F' => 'F', 'G' => 'G', 'H' => 'H', 'I' => 'I']);
             $form->ckeditor('parse', '詳解');
 
             $form->display('created_at', 'Created At');
@@ -210,10 +210,10 @@ class QuestionController extends Controller
             $form->display('id', 'ID');
             $form->hidden('qtype_id', 'Question Type')->default(2);
             $form->ckeditor('title', '題目');
-            $form->textarea('options', '選項')->default("A.選項內容\nB.選項內容\nC.選項內容\nD.選項內容\nE.選項內容");
+            $form->textarea('options', '選項')->default("A.選項內容\nB.選項內容\nC.選項內容\nD.選項內容\nE.選項內容\nF.選項內容\nG.選項內容\nH.選項內容\nI.選項內容");
 
             if (!is_null($id)) $form->display('answer', '原答案');
-            $form->multipleSelect('answer', '答案')->options(['A' => 'A', 'B' => 'B', 'C' => 'C', 'D' => 'D', 'E' => 'E']);
+            $form->multipleSelect('answer', '答案')->options(['A' => 'A', 'B' => 'B', 'C' => 'C', 'D' => 'D', 'E' => 'E' , 'F' => 'F', 'G' => 'G', 'H' => 'H', 'I' => 'I']);
                 // dd(implode(". ", json_decode(Question::where('id', $id)->value('answer'))));
             $form->ckeditor('parse', '詳解');
 
