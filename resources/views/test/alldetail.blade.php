8f644ddaee38dea1a29176edaebaba745e5ef4ce
wqqhou <30869335+wqqhou@users.noreply.github.com>
2020-04-08 19:32:42 +0800

    Update alldetail.blade.php

--- resources/views/test/alldetail.blade.php
+++ resources/views/test/alldetail.blade.php
@@ -4,9 +4,6 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
-                @unless($point==100)
-                    <a href={{"/test/$test_id/details"}} class="text-warning">點擊僅查看錯題</a>
-                @endunless
                 <h2 >答題詳情</h2>
                 @for($i = 0 ; $i < $total ; $i++)
                    @include('test.detail_form')
