77189743ef32537f03e343dc0eb932c6a678771f
wqqhou <30869335+wqqhou@users.noreply.github.com>
2020-04-08 19:18:14 +0800

    Update judge.blade.php

--- resources/views/test/judge.blade.php
+++ resources/views/test/judge.blade.php
@@ -8,6 +8,7 @@
                 <h4>感謝您的填寫!!</h4>
                 
                 <a class="text-warning" href={{"/test/$test_id/alldetails"}}>點擊查看答題詳情</a>
+                <a class="text-warning" href={{"/test"}}>繼續填答下一份</a>
             
         </div>
     </div>
